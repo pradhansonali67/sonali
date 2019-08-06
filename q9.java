41f695f681431723872554ae390c65737ba2df26
Sonali pradhan <53335321+pradhansonali67@users.noreply.github.com>
2019-08-06 13:04:19 +0530

    Add files via upload

---
+++ q9.java
@@ -0,0 +1,22 @@
+public class q9 {
+
+	public static void main(String[] args) {
+		double product;
+		for(int i=3;i<=1000;i++)
+		{
+			for(int j=i+1;j<1000;j++)
+			{
+				double c_square=Math.pow(i,2)+Math.pow(j,2);
+				double c=Math.sqrt(c_square);
+				if(i+j+c==1000)
+				{
+				product=i*j*c;
+				System.out.println((int)product);
+				break;
+				}
+			}
+		}
+
+	}
+
+}
